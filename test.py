460f5cc884430c065ba713ef9a5c6adb5cc5748b
h_muraka <murakamiputtyo@yahoo.co.jp>
2020-03-15 19:59:41 +0900

    create print_ten()

--- test.py
+++ test.py
@@ -7,8 +7,16 @@ def add(x, y):
     return x + y
 
 
+def print_ten():
+    for i in range(10):
+        print(i)
+
+
 def main():
     print("ok")
+    sum = add(4, 5)
+    print(sum)
+    print_ten()
 
 
 if __name__ == '__main__':
