85e252a7905c9cdd871a05f64663aa62a27f2d36
h_muraka <murakamiputtyo@yahoo.co.jp>
2020-03-25 13:42:28 +0900

    create sqrt

--- test.py
+++ test.py
@@ -3,23 +3,10 @@
 import numpy as np
 
 
-def add(x, y):
-    return x + y
-
-
-def print_ten():
-    for i in range(10):
-        print(i)
-
-
-def multi(x, y):
-    return x * y
-
-
 def main():
-    data = np.array([1, 2, 3, 4])
-    print(data)
-    # test
+    print("ok")
+    a = [np.sqrt(d) for d in range(10)]
+    print(a)
 
 
 if __name__ == '__main__':
