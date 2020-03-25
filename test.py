c5d1c15b63baf5ab4ccca25793777feaf1f14f93
h_muraka <murakamiputtyo@yahoo.co.jp>
2020-03-25 13:53:57 +0900

    make multi

--- test.py
+++ test.py
@@ -3,10 +3,15 @@
 import numpy as np
 
 
+def mult(a, b):
+    return a * b
+
+
 def main():
     print("ok")
     a = [np.sqrt(d) for d in range(10)]
     print(a)
+    mult(3, 4)
 
 
 if __name__ == '__main__':
