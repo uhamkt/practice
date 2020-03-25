ea984b54917a335f8439dd30fe0e01debdaf454f
h_muraka <murakamiputtyo@yahoo.co.jp>
2020-03-25 13:53:16 +0900

    make add func

--- test.py
+++ test.py
@@ -3,11 +3,16 @@
 import numpy as np
 
 
+def add(a, b):
+    return a + b
+
+
 def main():
     print("ok")
     a = [np.sqrt(d) for d in range(10)]
     print(a)
     print("data")
+    add(1, 2)
 
 
 if __name__ == '__main__':
