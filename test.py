048181f5bb30d695491ce38f9f8d951ca96a1647
h_muraka <murakamiputtyo@yahoo.co.jp>
2020-05-05 21:44:37 +0900

    initialize commit

--- test.py
+++ test.py
@@ -3,28 +3,8 @@
 import numpy as np
 
 
-def add(x, y):
-    return x + y
-
-class Test():
-    def __init__(self, data):
-        self.data = data
-
-def print_ten():
-    for i in range(10):
-        print(i)
-
-
-def multi(x, y):
-    return x * y
-
-
 def main():
-    data = np.array([1, 2, 3, 4])
-    print(data)
-    # test
-    print("take out")
-
+    a = 1
 
 if __name__ == '__main__':
     main()
